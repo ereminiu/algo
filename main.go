ceeb35bb1280278b71bde7e597e161ebf037de51
Ivan Eremin <ereminiu@mail.ru>
2023-03-22 10:27:24 +0300

    add time measuring for segtree

--- main.go
+++ main.go
@@ -3,57 +3,36 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 
-	"github.com/ereminiu/algo/segmenttree"
+	segtree "github.com/ereminiu/algo/segmenttree"
 	"github.com/ereminiu/algo/utils"
 )
 
-func testSegTree(times int) {
-	n := 1000
-	for it := 0; it < times; it++ {
+func averageTimeSegtree(times int) time.Duration {
+	start := time.Now()
+
+	for it := 0; it < 100; it++ {
+		n := 15000
 		a := utils.RandSlice(n)
-		st := segmenttree.NewTree(&a)
-
-		getmax := func(l, r int) int {
-			ret := 0
-			for i := l; i < r; i++ {
-				ret = utils.Max(ret, a[i])
+		st := segtree.NewTree(&a)
+		k := rand.Intn(100)
+		if k%2 == 0 {
+			idx := rand.Intn(n)
+			st.Update(idx, 19)
+			a[idx] = 19
+		} else {
+			l, r := rand.Intn(n-1), rand.Intn(n)
+			for r <= l {
+				r = rand.Intn(n)
 			}
-			return ret
+			_ = st.GetMax(l, r)
 		}
-
-		for rep := 0; rep < 100; rep++ {
-			k := rand.Intn(100)
-			if k%2 == 0 {
-				idx := rand.Intn(n)
-				st.Update(idx, 19)
-				a[idx] = 19
-			} else {
-				l, r := rand.Intn(n-1), rand.Intn(n)
-				for r <= l {
-					r = rand.Intn(n)
-				}
-				if getmax(l, r) != st.GetMax(l, r) {
-					fmt.Printf("Failed on %v %v %v\n", a, l, r)
-					fmt.Printf("Should be %v instead of %v", getmax(l, r), st.GetMax(l, r))
-					panic("Aboba")
-				}
-			}
-		}
-
 	}
-	fmt.Printf("Tests passed :)\n")
+
+	return time.Now().Sub(start)
 }
 
 func main() {
-	// a := []int{1, 2, 3, -2, 7, 8}
-	// st := segmenttree.NewTree(&a)
-
-	// fmt.Println(st.GetMax(0, 5))
-	// st.Update(0, 29)
-	// fmt.Println(st.GetMax(0, 5))
-
-	// fmt.Println(st.GetMax())
-
-	testSegTree(1200)
+	fmt.Println(averageTimeSegtree(12))
 }
