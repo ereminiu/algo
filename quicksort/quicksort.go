46ce52bc79be47ba899ac2cfd4cab4e2ccb33bc2
Ivan Eremin <ereminiu@mail.ru>
2023-03-30 19:23:32 +0300

    Quicksort refactoring

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -1,10 +1,10 @@
 package quicksort
 
-func Sort(a *[]int) {
-	quicksort(0, len(*a)-1, a)
+func Sort(a []int) {
+	quicksort(0, len(a)-1, a)
 }
 
-func quicksort(l, r int, a *[]int) {
+func quicksort(l, r int, a []int) {
 	if l >= r {
 		return
 	}
@@ -13,18 +13,18 @@ func quicksort(l, r int, a *[]int) {
 	quicksort(q+1, r, a)
 }
 
-func partition(l, r int, a *[]int) int {
-	v := (*a)[(l+r)/2]
+func partition(l, r int, a []int) int {
+	v := (a)[(l+r)/2]
 	i, j := l, r
 	for i <= j {
-		for (*a)[i] < v {
+		for a[i] < v {
 			i++
 		}
-		for (*a)[j] > v {
+		for a[j] > v {
 			j--
 		}
 		if i < j {
-			(*a)[i], (*a)[j] = (*a)[j], (*a)[i]
+			a[i], a[j] = a[j], a[i]
 			i++
 			j--
 		}
