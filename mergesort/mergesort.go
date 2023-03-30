1887983a106f6ed1ce81fb6aac75fc01e51f8a35
Ivan Eremin <ereminiu@mail.ru>
2023-03-30 19:43:23 +0300

    refactor mergesort

--- mergesort/mergesort.go
+++ mergesort/mergesort.go
@@ -1,27 +1,27 @@
 package mergesort
 
-func merge(a *[]int, b *[]int) []int {
-	n, m := len(*a), len(*b)
+func merge(a []int, b []int) []int {
+	n, m := len(a), len(b)
 	i, j, k := 0, 0, 0
 	ret := make([]int, n+m)
 	for k < n+m {
 		if i == n {
-			ret[k] = (*b)[j]
+			ret[k] = b[j]
 			j++
 			k++
 			continue
 		} else if j == m {
-			ret[k] = (*a)[i]
+			ret[k] = a[i]
 			i++
 			k++
 			continue
 		}
 
-		if (*a)[i] < (*b)[j] {
-			ret[k] = (*a)[i]
+		if a[i] < b[j] {
+			ret[k] = a[i]
 			i++
 		} else {
-			ret[k] = (*b)[j]
+			ret[k] = b[j]
 			j++
 		}
 		k++
@@ -29,17 +29,17 @@ func merge(a *[]int, b *[]int) []int {
 	return ret
 }
 
-func devide(nums *[]int, l, r int) []int {
+func devide(nums []int, l, r int) []int {
 	if l == r-1 {
-		return []int{(*nums)[l]}
+		return []int{nums[l]}
 	}
 	mid := (l + r) / 2
 	left := devide(nums, l, mid)
 	right := devide(nums, mid, r)
-	return merge(&left, &right)
+	return merge(left, right)
 }
 
 // usage: a = Sort(&a) where a is int slice
-func Sort(nums *[]int) []int {
-	return devide(nums, 0, len(*nums))
+func Sort(nums []int) []int {
+	return devide(nums, 0, len(nums))
 }
